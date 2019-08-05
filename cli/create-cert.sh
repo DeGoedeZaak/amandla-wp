66428e710d8a858930cfef2e7b2eb55e41b8b635
gordan43dom <gordankondev@yahoo.com>
2019-08-05 15:50:16 +0200

    Detect os type for generating ssh keys

--- cli/create-cert.sh
+++ cli/create-cert.sh
@@ -8,6 +8,45 @@ BLUE='\033[0;34m'
 YELLOW='\033[0;93m'
 NC='\033[0m'
 
+
+OS=`uname -s`
+
+if [ "$OS" = "Darwin" ]; then
+    openssl req \
+        -newkey rsa:2048 \
+        -x509 \
+        -nodes \
+        -keyout myapp.local.key \
+        -new \
+        -out myapp.local.crt \
+        -subj /CN=myapp.local \
+        -reqexts SAN \
+        -extensions SAN \
+        -config <(cat /System/Library/OpenSSL/openssl.cnf \
+            <(printf '[SAN]\nsubjectAltName=DNS:myapp.local')) \
+        -sha256 \
+        -days 3650
+
+elif [ "$OS" = "Linux" ]; then
+    openssl req \
+        -newkey rsa:2048 \
+        -x509 \
+        -nodes \
+        -keyout myapp.local.key \
+        -new \
+        -out myapp.local.crt \
+        -subj /CN=myapp.local \
+        -reqexts SAN \
+        -extensions SAN \
+        -config <(cat /usr/lib/ssl/openssl.cnf \
+            <(printf '[SAN]\nsubjectAltName=DNS:myapp.local')) \
+        -sha256 \
+        -days 3650
+else
+    echo "Failed to identify this OS"
+    exit -1
+fi
+
 openssl req \
     -newkey rsa:2048 \
     -x509 \
