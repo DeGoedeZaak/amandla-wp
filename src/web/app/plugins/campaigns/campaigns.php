6498b2e56aa8f5074c1726235fb5a4db9f888072
corewaysolution <info@corewaysolution.com>
2019-08-14 18:46:10 +0530

    uploaded all files from local to git

---
+++ src/web/app/plugins/campaigns/campaigns.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * @package Campaigns
+ * @version 0.1
+ */
+
+/*
+  Plugin Name: Campaigns
+  Description: Pull and lists petitions from CSL and Speakout
+  Author: Hiemanshu Sharma
+  Version: 0.1
+  Text Domain: campaigns
+*/
+
+if ( ! defined('CAMPAIGNS_BASE_DIR') ) {
+    define('CAMPAIGNS_BASE_DIR', trailingslashit(plugin_dir_path(__FILE__)));
+}
+
+register_activation_hook(__FILE__, 'campaigns_activation_hook');
+register_deactivation_hook(__FILE__, 'campaigns_deactivation_hook');
+
+add_action('admin_menu', 'campaigns_admin_menu');
+add_action('campaigns_sync_event', function() {
+    require_once(CAMPAIGNS_BASE_DIR . 'lib/sync.php');
+    sync_campaigns();
+});
+
+add_action('init', function() {
+    require_once(CAMPAIGNS_BASE_DIR . 'lib/campaign.php');
+    Campaign::create_post_type();
+});
+
+function campaigns_activation_hook() {
+    require_once(CAMPAIGNS_BASE_DIR . 'lib/sync.php');
+    setup_sync();
+}
+
+function campaigns_deactivation_hook() {
+    require_once(CAMPAIGNS_BASE_DIR . 'lib/sync.php');
+    remove_sync();
+}
+
+function campaigns_admin_menu() {
+    add_menu_page('Campaigns Plugin Settings', 'Campaigns Settings', 'administrator', __FILE__, 'campaigns_settings_page');
+
+    add_action('admin_init', 'campaigns_admin_init');
+}
+
+function campaigns_admin_init() {
+    register_setting('campaigns-settings', 'speakout_url');
+    register_setting('campaigns-settings', 'csl_url');
+}
+
+function campaigns_settings_page() {
+?>
+    <div class="wrap">
+      <h1>Campaigns Settings Page</h1>
+      <form method="post" action="options.php">
+        <?php settings_fields( 'campaigns-settings' ); ?>
+        <?php do_settings_sections( 'campaigns-settings' ); ?>
+
+        <table class="form-table">
+            <tr valign="top">
+            <th scope="row">Speakout URL</th>
+            <td><input type='text' name='speakout_url' value="<?php echo esc_attr(get_option('speakout_url')) ?>" style="width: 60%;"></td>
+            </tr>
+        </table>
+
+        <table class="form-table">
+            <tr valign="top">
+            <th scope="row">CSL URL</th>
+            <td><input type='text' name='csl_url' value="<?php echo esc_attr(get_option('csl_url')) ?>" style="width: 60%;"></td>
+            </tr>
+        </table>
+
+        <?php submit_button(); ?>
+      </form>
+    </div>
+<?php
+}
+?>
---
+++ src/web/app/plugins/campaigns/lib/campaign.php
@@ -0,0 +1,88 @@
+<?php
+
+class Campaign {
+
+    static public function add_or_update($campaign_data) {
+        $posts = new WP_Query([
+            'post_type' => 'campaign',
+            'meta_query' => [
+                'relation' => 'AND',
+                [
+                    'key' => 'external_id',
+                    'value' => $campaign_data["external_id"],
+                    'compare' => '='
+                ],
+                [
+                    'key' => 'source',
+                    'value' => $campaign_data["source"],
+                    'compare' => '='
+                ]
+            ]
+        ]);
+
+        $post_content = (empty($campaign_data["description"])) ? "" : $campaign_data["description"];
+
+        $post_args = [
+            'post_type' => 'campaign',
+            'post_title' => $campaign_data["name"],
+            'post_content' => $post_content,
+            'post_status' => 'publish',
+            'post_date' => $campaign_data["created_at"],
+            'comment_status' => 'closed',
+            'ping_status' => 'closed',
+            'meta_input' => [
+                'external_id' => $campaign_data["external_id"],
+                'url' => $campaign_data["url"],
+                'source' => $campaign_data["source"],
+                'image' => $campaign_data["image"],
+                'actions' => $campaign_data["actions"],
+                'max_actions' => $campaign_data["max_actions"]
+            ]
+        ];
+
+        if ( $posts->have_posts() ) {
+            $posts->the_post();
+            wp_update_post(array_merge([
+                'ID' => get_the_ID()
+            ], $post_args));
+        } else {
+            wp_insert_post($post_args);
+        }
+
+        wp_reset_postdata();
+    }
+
+    static public function create_post_type() {
+        register_post_type(
+            'campaign',
+            [
+                'labels' => [
+                    'name' => __('Campaigns'),
+                    'singular_name' => __('Campaign'),
+                    'add_new' => _x('Add New', 'campaign'),
+                    'add_new_item' => __('Add New Campaign'),
+                    'edit_item' => __('Edit Campaign'),
+                    'new_item' => __('New Campaign'),
+                    'view_item' => __('View Campaign'),
+                    'search_items' => __('Search Campaigns'),
+                    'not_found' =>  __('Nothing found'),
+                    'not_found_in_trash' => __('Nothing found in Trash')
+                ],
+                'description' => 'Campaigns',
+                'public' => true,
+                'show_in_menu' => true,
+                'menu_position' => 20,
+                'has_archive' => true,
+                'supports' => [
+                    'title',
+                    'editor',
+                    'thumbnail',
+                    'custom-fields'
+                ]
+            ]
+        );
+    }
+
+}
+
+?>
